c8d1a525fac264cefab11f0984744c3aacf23199
Pea <pea@misfist.com>
2019-03-21 23:51:33 -0400

    Modified index.php to display list of posts.

--- index.php
+++ index.php
@@ -3,19 +3,29 @@ get_header(); ?>
 
 <div class="content content_template_base1">
 
-	<?php
-	if ( have_posts() ):
-		while ( have_posts() ):
-			the_post();
-			?>
-		<h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
-		<?php
-		endwhile;
+<?php
+		if ( have_posts() ) {
 
-	else :
-		get_template_part( 'template-parts/content/content', 'none' );
-	endif; ?>
+			// Load posts loop.
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content/content', 'excerpt' );
+			}
+
+		} else {
+
+			// If no content, include the "No posts found" template.
+			get_template_part( 'template-parts/content/content', 'none' );
+
+		}
+		?>
+
+		<div class="pagination">
+			<?php 
+			// Previous/next page navigation.
+			echo paginate_links();
+			?>
+		</div>
 
 </div>
 
